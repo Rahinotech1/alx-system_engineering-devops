516dba2bf2b8b2a6dfb0e160c8081af86c03f3a2
Rahinotech1 <99316887+Rahinotech1@users.noreply.github.com>
2022-09-16 20:29:06 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# Download puppet-lint 2.1.1
+# Download puppet-lint 2.1.0
 exec { 'apt-get update':
   command => '/usr/bin/apt-get update'
 }
@@ -9,7 +9,7 @@ package { 'rubygems1.9.1':
 }
 
 package { 'puppet-lint':
-  ensure   => '2.1.1'
+  ensure   => '2.1.0'
   require  => Package['rubygems1.9.1'],
   provider => 'gem'
 }
