b1eedf86fcc6e935e876df165114dcc049a06ba4
Rahinotech1 <99316887+Rahinotech1@users.noreply.github.com>
2022-11-15 16:25:18 +0000

    Functions that querries

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-"""script for parsing web data from an api
-"""
-import json
+"""queries the Reddit API"""
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """api call to reddit to get the number of subscribers
-    """
-    base_url = 'https://www.reddit.com/r/'
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    # grab info about all users
-    url = base_url + '{}/top/.json?count=10'.format(subreddit)
-    response = requests.get(url, headers=headers)
-    resp = json.loads(response.text)
+    """returns first 10 posts"""
+    header = {"User-Agent": "Holberton"}
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=10"
+    r = requests.get(url, headers=header, allow_redirects=False)
+    if r.status_code == 200:
+        for i in r.json().get("data", None).get("children", None):
+            print(i.get("data", None).get("title", None))
+    else:
+        print(None)
 
-    try:
-        # grab the info about the users' tasks
-        data = resp.get('data')
-        children = data.get('children')
-    except:
-        print('None')
-    if children is None or data is None or len(children) < 1:
-        print('None')
-
-    for i, post_dict in enumerate(children):
-        if i == 10:
-            break
-        print(post_dict.get('data').get('title'))
+if __name__ == "__main__":
+    pass
