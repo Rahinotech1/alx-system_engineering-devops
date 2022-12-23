56cfae6b49b8cbb523c372e13dde9cf22422aec7
Rahinotech1 <99316887+Rahinotech1@users.noreply.github.com>
2022-12-23 18:21:06 +0000

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,12 @@
-# Puppet
-exec { 'fixer':
-    command => 'sed -i s/15/1024 /etc/default/nginx',
-    path    => '/bin',
+# Set ulimit on nginx max file descriptors to system soft limit
+exec {'ulimit':
+  command => 'sed -r -i "s/(ULIMIT=\"-n) [0-9]+/\1 8000/" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -e /etc/default/nginx',
 }
-service { 'nginx':
-    ensure    => running,
-    subscribe => Exec['fixer'],
+
+exec {'restart nginx':
+  command => 'service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  require => Exec['ulimit'],
 }
